d35ed4d7e8a4569e2e4be365fbb6ef73bbbe8d08
David <David@172.250.250.43>
2012-09-03 13:42:01 -0600

    First Tank Drive Commit

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -4,11 +4,12 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
 package edu.wpi.first.wpilibj.templates;
 
-
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SimpleRobot;
+import edu.wpi.first.wpilibj.Timer;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -18,17 +19,28 @@ import edu.wpi.first.wpilibj.SimpleRobot;
  * directory.
  */
 public class RobotTemplate extends SimpleRobot {
+
+    RobotDrive drive;
+    Joystick leftStick;
+    Joystick rightStick;
+
     /**
      * This function is called once each time the robot enters autonomous mode.
      */
     public void autonomous() {
-        
     }
 
     /**
      * This function is called once each time the robot enters operator control.
      */
     public void operatorControl() {
+        drive = new RobotDrive(1, 2);
+        leftStick = new Joystick(1);
+        rightStick = new Joystick(2);
 
+        while (true && isOperatorControl() && isEnabled()) {
+            drive.tankDrive(leftStick, rightStick); // drive with joysticks
+            Timer.delay(0.005);
+        }
     }
 }
