ca3cbda337efd23c563c7c93baa79ce666cf97c5
alex <alex@172.250.250.54>
2013-03-01 06:41:13 -0700

    Control update

--- FinalRobotControl/src/edu/wpi/first/wpilibj/templates/FinalRobot.java
+++ FinalRobotControl/src/edu/wpi/first/wpilibj/templates/FinalRobot.java
@@ -13,10 +13,14 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Gyro;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.tables.TableKeyNotDefinedException;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -39,6 +43,7 @@ public class FinalRobot extends IterativeRobot {
     Victor arm2 = new Victor(6);
     Joystick joy1 = new Joystick(1);
     Joystick joy2 = new Joystick(2);
+    Joystick joy3 = new Joystick(3);
     Victor launch = new Victor(7);
     Victor injector = new Victor(8);
     Compressor comp = new Compressor(1, 1);
@@ -46,7 +51,7 @@ public class FinalRobot extends IterativeRobot {
     boolean wheel = false;
     LatchedBoolean hoppercontrol = new LatchedBoolean();
     boolean fireControl = false;
-    int fireState;
+    boolean firing = false;
     double injPwr;
     double launchPwr;
     double injPulsePwr;
@@ -56,8 +61,23 @@ public class FinalRobot extends IterativeRobot {
     double injPulseDel;
     double launchPulseDel;
     double feedTime;
-    double startTime;
+    double fireStartTime;
     boolean defaultPistonState;
+    double autoStartTime;
+    int autoState = 0;
+    SendableChooser autoChooser = new SendableChooser();
+    PIDController armpid = new PIDController(1, 1, 1, gyro, arm2);
+    Preferences pref = Preferences.getInstance();
+    Object autoMode;
+
+    {
+        armpid.setInputRange(-180, 180);
+        armpid.setOutputRange(-1, 1);
+        autoChooser.addDefault("None", null);
+        autoChooser.addObject("Behind Pyramid", new Double(-4));
+        autoChooser.addObject("Back Corner Of Pyramid", new Double(-3));
+        autoChooser.addObject("Front Corner Of Pyramid", new Double(-5));
+    }
 
     public void robotInit() {
         drive.setSafetyEnabled(false);
@@ -76,18 +96,45 @@ public class FinalRobot extends IterativeRobot {
         SmartDashboard.putBoolean("Piston Default Extended", false);
         SmartDashboard.putNumber("Shoulder Power", 100);
         SmartDashboard.putNumber("Belt Power", 100);
+        SmartDashboard.putNumber("kP", pref.getDouble("kp", 1));
+        SmartDashboard.putNumber("kI", pref.getDouble("kI", 1));
+        SmartDashboard.putNumber("kD", pref.getDouble("kD", 1));
         double voltage = DriverStation.getInstance().getBatteryVoltage();
         //magic code to calculate percent charge from voltage. formula given by quartic regression on empirical data. DO NOT TOUCH
         //DO NOT TOUCH NEXT LINE
-        SmartDashboard.putNumber(" initial charge percent", (voltage > 11.7?(voltage < 12.6?337.90418311119*MathUtils.pow(voltage, 4)-16321.796968975*MathUtils.pow(voltage, 3)+295645.26274574*MathUtils.pow(voltage, 2)-2379991.6548433*voltage+7184291.5749442:125):-10));
+        SmartDashboard.putNumber(" initial charge percent", (voltage > 11.7 ? (voltage < 12.6 ? 337.90418311119 * MathUtils.pow(voltage, 4) - 16321.796968975 * MathUtils.pow(voltage, 3) + 295645.26274574 * MathUtils.pow(voltage, 2) - 2379991.6548433 * voltage + 7184291.5749442 : 125) : -10));
         //DO NOT TOUCH PREVIOUS LINE
+    }
+
+    public void autonomousInit() {
+        wheel = true;
+        armpid.setPID(SmartDashboard.getNumber("kP"), SmartDashboard.getNumber("kI"), SmartDashboard.getNumber("kD"));
+        autoMode = autoChooser.getSelected();
+        if (autoMode != null) {
+            armpid.setSetpoint(((Double) autoMode).doubleValue());
+            armpid.enable();
+        }
     }
 
     /**
      * This function is called periodically during autonomous
      */
     public void autonomousPeriodic() {
-        drive.setLeftRightMotorOutputs(0.1, 0.1);
+        if (autoMode != null) {
+            iterateFiring();
+            if (Timer.getFPGATimestamp() - autoStartTime > 5 && autoState == 0) {
+                autoState = 1;
+                fireControl = true;
+            }
+            if (Timer.getFPGATimestamp() - autoStartTime > 8.5 && autoState == 1) {
+                autoState = 2;
+                fireControl = true;
+            }
+            if (Timer.getFPGATimestamp() - autoStartTime > 11 && autoState == 2) {
+                autoState = 3;
+                fireControl = true;
+            }
+        }
     }
 
     /**
@@ -115,15 +162,15 @@ public class FinalRobot extends IterativeRobot {
          *      depends on hardware not installed; unknown effect, possibilities:
          *          nothing
          *          unpredictable direction mapping
-         * joystick 2, button 3(top buttons): rotate shoulder forward
+         * joystick 3, button 6(bottom buttons): rotate shoulder forward
          *      this control may be added to gunner's joysticks to reduce the driver's need to multitask
          *      may be remapped if awkward to control.
-         * joystick 2, button 4(top buttons): rotate shoulder backward
+         * joystick 3, button 7(bottom buttons): rotate shoulder backward
          *      this control may be added to gunner's joysticks to reduce the driver's need to multitask
          *      may be remapped if awkward to control.
-         * joystick 2, button 5(top buttons): raise climbing claws
+         * joystick 3, button 11(bottom buttons): raise climbing claws
          *      may be remapped if awkward to control.
-         * joystick 2, button 6(top buttons): lower climbing claws
+         * joystick 3, button 10(bottom buttons): lower climbing claws
          *      may be remapped if awkward to control.
          * additional notes: be cautious about initiating a firing cycle while moving or accelerating rapidly,
          *          this causes a large voltage drop with possible side effects including:
@@ -143,21 +190,21 @@ public class FinalRobot extends IterativeRobot {
 //        System.out.println("test");
 //        System.out.println("(" + accel.getAcceleration(Axes.kX) + ", " + accel.getAcceleration(Axes.kY) + ", " + accel.getAcceleration(Axes.kZ) + ", " + gyro.getAngle() + ")");
 
-        if (!driveLock && joy1.getRawButton(9)) {
-//            System.out.println(driver);
-            driver = !driver;
-            driveLock = true;
-//            System.out.println("driver: "+driver+" "+!driver);
-            if (driver == false) {
-                heading = gyro.getAngle();
-            }
-//            System.out.println("toggle: " + driver);
-        } else if (driveLock && !joy1.getRawButton(9)) {
-            driveLock = false;
-        }
+//        if (!driveLock && joy1.getRawButton(9)) {
+////            System.out.println(driver);
+////            driver = !driver;
+//            driveLock = true;
+////            System.out.println("driver: "+driver+" "+!driver);
+//            if (driver == false) {
+//                heading = gyro.getAngle();
+//            }
+////            System.out.println("toggle: " + driver);
+//        } else if (driveLock && !joy1.getRawButton(9)) {
+//            driveLock = false;
+//        }
         if (driver) {
 //            System.out.println("trigger");
-            absCtrlMode.set(joy1.getRawButton(8));
+//            absCtrlMode.set(joy1.getRawButton(8));
             drive.mecanumDrive_Cartesian(joy2.getX(), -joy2.getY(), joy1.getX(), absCtrlMode.getValue() ? gyro.getAngle() : 0);
         } else {
             if (joy2.getRawButton(4) && !joy2.getRawButton(5)) {
@@ -177,16 +224,16 @@ public class FinalRobot extends IterativeRobot {
             heading += joy2.getAxis(Joystick.AxisType.kX);
             drive.mecanumDrive_Cartesian(gunnerX, gunnerY, (-gyro.getAngle() + heading) / 20, 0);
         }
-        if (joy2.getRawButton(11)) {
+        if (joy3.getRawButton(11)) {
             arm.set(-SmartDashboard.getNumber("Belt Power") / 100);
-        } else if (joy2.getRawButton(10)) {
+        } else if (joy3.getRawButton(10)) {
             arm.set(SmartDashboard.getNumber("Belt Power") / 100);
         } else {
             arm.set(0);
         }
-        if (joy2.getRawButton(6) || joy2.getRawButton(3)) {
+        if (joy3.getRawButton(6) /*|| joy2.getRawButton(3)*/) {
             arm2.set(SmartDashboard.getNumber("Shoulder Power") / 100);
-        } else if (joy2.getRawButton(7) || joy2.getRawButton(2)) {
+        } else if (joy3.getRawButton(7)/* || joy2.getRawButton(2)*/) {
             arm2.set(-SmartDashboard.getNumber("Shoulder Power") / 100);
         } else {
             arm2.set(0);
@@ -196,7 +243,7 @@ public class FinalRobot extends IterativeRobot {
             injPwr = -SmartDashboard.getNumber("Injector Power") / 100;
             if (joy1.getRawButton(7)) {
                 feed.set(DoubleSolenoid.Value.kReverse);
-            } else if(joy1.getRawButton(6)) {
+            } else if (joy1.getRawButton(6)) {
                 feed.set(DoubleSolenoid.Value.kForward);
             } else {
                 feed.set(DoubleSolenoid.Value.kOff);
@@ -207,6 +254,34 @@ public class FinalRobot extends IterativeRobot {
         } else if (joy1.getRawButton(2)) {
             wheel = false;
         }
+        iterateFiring();
+        if (joy1.getRawButton(10)) {
+            comp.stop();
+        }
+        if (joy2.getRawButton(11)) {
+            comp.start();
+        }
+    }
+
+    public void testInit() {
+        pref.putDouble("kP", SmartDashboard.getNumber("kP", 1));
+        pref.putDouble("kI", SmartDashboard.getNumber("kI", 1));
+        pref.putDouble("kD", SmartDashboard.getNumber("kD", 1));
+        pref.save();
+    }
+
+    /**
+     * This function is called periodically during test mode
+     */
+    public void testPeriodic() {
+        double voltage = DriverStation.getInstance().getBatteryVoltage();
+        //magic code to calculate percent charge from voltage. formula given by quartic regression on empirical data. DO NOT TOUCH
+        //DO NOT TOUCH NEXT LINE
+        SmartDashboard.putNumber(" initial charge percent", (voltage > 11.7 ? (voltage < 12.6 ? 337.90418311119 * MathUtils.pow(voltage, 4) - 16321.796968975 * MathUtils.pow(voltage, 3) + 295645.26274574 * MathUtils.pow(voltage, 2) - 2379991.6548433 * voltage + 7184291.5749442 : 125) : -10));
+        //DO NOT TOUCH PREVIOUS LINE
+    }
+
+    private void iterateFiring() throws TableKeyNotDefinedException {
         if (wheel && !fireControl) {
             launch.set(launchPwr);
             injector.set(injPwr);
@@ -214,16 +289,10 @@ public class FinalRobot extends IterativeRobot {
             launch.set(0);
             injector.set(0);
         }
-        if (joy1.getRawButton(10)) {
-            comp.stop();
-        }
-        if (joy2.getRawButton(11)) {
-            comp.start();
-        }
-        if (joy1.getRawButton(1) && !fireControl) {
+        if (joy1.getRawButton(1) && !fireControl || fireControl && !firing) {
             System.out.println("fireStart");
-            injPulsePwr = -SmartDashboard.getNumber("Injector Pulse Power")/100;
-            launchPulsePwr = -SmartDashboard.getNumber("Launcher Pulse Power")/100;
+            injPulsePwr = -SmartDashboard.getNumber("Injector Pulse Power") / 100;
+            launchPulsePwr = -SmartDashboard.getNumber("Launcher Pulse Power") / 100;
             injPulseLen = SmartDashboard.getNumber("Injector Pulse Length");
             launchPulseLen = SmartDashboard.getNumber("Launcher Pulse Length");
             injPulseDel = SmartDashboard.getNumber("Injector Pulse Delay");
@@ -232,13 +301,13 @@ public class FinalRobot extends IterativeRobot {
             defaultPistonState = SmartDashboard.getBoolean("Piston Default Extended");
             feed.set(DoubleSolenoid.Value.kReverse);
             fireControl = true;
-            fireState = 0;
-            startTime = Timer.getFPGATimestamp()+(defaultPistonState?feedTime:0);
+            fireStartTime = Timer.getFPGATimestamp() + (defaultPistonState ? feedTime : 0);
+            firing = true;
         }
         if (fireControl) {
             System.out.println("firing");
             double currTime = Timer.getFPGATimestamp();
-            double fireTime = currTime - startTime;
+            double fireTime = currTime - fireStartTime;
             if (fireTime >= 0) {
                 feed.set(DoubleSolenoid.Value.kForward);
             }
@@ -255,25 +324,15 @@ public class FinalRobot extends IterativeRobot {
                 launch.set(launchPwr);
             }
             if (fireTime >= feedTime) {
-                    feed.set(defaultPistonState?DoubleSolenoid.Value.kOff:DoubleSolenoid.Value.kReverse);
+                feed.set(defaultPistonState ? DoubleSolenoid.Value.kOff : DoubleSolenoid.Value.kReverse);
             }
-            if (fireTime >= Math.max(Math.max(injPulseDel + injPulseLen, launchPulseDel + launchPulseLen),feedTime+0.1)) {
+            if (fireTime >= Math.max(Math.max(injPulseDel + injPulseLen, launchPulseDel + launchPulseLen), feedTime + 0.1)) {
                 if (!joy1.getRawButton(1)) {
                     fireControl = false;
+                    firing = false;
                     feed.set(DoubleSolenoid.Value.kOff);
                 }
             }
         }
-    }
-
-    /**
-     * This function is called periodically during test mode
-     */
-    public void testPeriodic() {
-        double voltage = DriverStation.getInstance().getBatteryVoltage();
-        //magic code to calculate percent charge from voltage. formula given by quartic regression on empirical data. DO NOT TOUCH
-        //DO NOT TOUCH NEXT LINE
-        SmartDashboard.putNumber(" initial charge percent", (voltage > 11.7?(voltage < 12.6?337.90418311119*MathUtils.pow(voltage, 4)-16321.796968975*MathUtils.pow(voltage, 3)+295645.26274574*MathUtils.pow(voltage, 2)-2379991.6548433*voltage+7184291.5749442:125):-10));
-        //DO NOT TOUCH PREVIOUS LINE
     }
 }
