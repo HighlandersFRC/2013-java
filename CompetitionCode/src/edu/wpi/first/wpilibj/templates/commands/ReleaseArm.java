1e6c844e172cf2afec38d1482e00ab05f02a1c36
alex <alex@172.250.250.54>
2013-04-21 14:17:27 -0600

    fixed inverted shoulder control

--- CompetitionCode/src/edu/wpi/first/wpilibj/templates/commands/ReleaseArm.java
+++ CompetitionCode/src/edu/wpi/first/wpilibj/templates/commands/ReleaseArm.java
@@ -23,7 +23,7 @@ public class ReleaseArm extends CommandBase {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        climberShoulder.setShoulder(oi.shoulderControl.get());
+        climberShoulder.setShoulder(-oi.shoulderControl.get());
     }
 
     // Make this return true when this Command no longer needs to run execute()
